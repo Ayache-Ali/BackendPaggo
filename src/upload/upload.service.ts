5c064d6d13c9226645be2044383cfb3f5b234aef
Ayache-Ali <aluxe02@hotmail.com>
2025-03-26 16:47:56 -0300

    modified:   src/upload/upload.service.ts

--- src/upload/upload.service.ts
+++ src/upload/upload.service.ts
@@ -1,26 +1,21 @@
 import { Injectable, NotFoundException } from '@nestjs/common';
 import { PrismaService } from '../prisma/prisma.service';
-import axios from 'axios';
+import { createWorker } from 'tesseract.js';
 import OpenAI from "openai";
+
 
 @Injectable()
 export class UploadService {
   constructor(private prisma: PrismaService) {}
 
   private async extractTextFromImage(fileBuffer: Buffer): Promise<string> {
-    const apiKey = 'K82981710988957'; // Coloque a chave da API OCR.space aqui
-    const formData = new FormData();
-    formData.append('base64Image', fileBuffer.toString('base64')); // Converte o buffer para base64
+    const worker = await createWorker(); 
+    await worker.load("por")
 
-    const response = await axios.post('https://api.ocr.space/parse/image', formData, {
-      headers: {
-        'apikey': apiKey,
-        'Content-Type': 'multipart/form-data',
-      }
-    });
+    const { data } = await worker.recognize(fileBuffer);
+    await worker.terminate(); 
 
-    const parsedText = response.data.ParsedResults[0]?.ParsedText || '';
-    return parsedText;
+    return data.text || "";
   }
 
   async getChatExplanation(id: string, apiKey: string): Promise<string> {
@@ -42,10 +37,9 @@ export class UploadService {
 
     const response = await openai.chat.completions.create({
       model: "gpt-4o",
-      messages: [{ role: "user", content: `Explique este texto: ${file.text}` }], 
+      messages: [{ role: "user", content: `Explique este texto: ${file.text}` }],
       max_tokens: 200,
     });
-
     const explanation = response.choices[0].message?.content?.trim() || '';
 
     await this.prisma.document.update({
@@ -54,9 +48,11 @@ export class UploadService {
     });
 
     return explanation;
+
   }
 
   async saveFile(userId: string, filename: string, fileBuffer: Buffer) {
+
     const extractedText = await this.extractTextFromImage(fileBuffer);
 
     return this.prisma.document.create({
@@ -88,4 +84,6 @@ export class UploadService {
       },
     });
   }
+
+  
 }
